8bead709ded7fa22d8080ff8afcfc41a6cdf3794
Einar Alex <einaralex@Einars-MacBook-Pro.local>
2018-10-17 00:13:12 +0000

    Moved icons and styled footer

---
+++ assets/index.js
@@ -0,0 +1,17 @@
+import Clock from "./icons/clock.svg";
+import Chat from "./icons/chat.svg";
+import Chat2 from "./icons/chat2.svg";
+import Facebook from "./icons/facebook-primary.svg";
+import Mail from "./icons/mail.svg";
+import Phone from "./icons/phone.svg";
+import Youtube from "./icons/youtube-primary.svg";
+
+export {
+  Chat,
+  Chat2,
+  Clock,
+  Facebook,
+  Mail,
+  Phone,
+  Youtube
+};
--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, Menu } from "semantic-ui-react";
 import styles from "./Footer.scss"
+import { Chat, Chat2, Clock, Facebook, Mail, Phone, Youtube } from "../../../assets"
 
 
 export default (props) => {
@@ -11,36 +12,42 @@ export default (props) => {
     <Grid.Column width={16}>
         <Grid>
         <Grid.Row centered className={styles.top}>
-          <Grid.Column width={11} textAlign='left' verticalAlign='middle'>
+          <Grid.Column width={10} textAlign='left' verticalAlign='middle'>
             <h4>GETUM VIÐ AÐSTOÐAÐ ÞIG?</h4><br />
-               <i></i>
+               <img src={Clock} alt="Opnunartími" align="middle"/>
                <span>09:00 - 16:30</span>
-               <i></i>
+               <img src={Chat} alt="Netspjall" align="middle" />
                <span>Netspjall</span>
-               <i></i>
+               <img src={Phone} alt="Símanúmer" align="middle" />
                <span>514 1000</span>
-               <i></i>
+               <img src={Mail} alt="Póstur" align="middle" />
                <span>vordur@vordur.is</span>
-               <i></i>
+               <img src={Chat2} alt="Senda fyrirspurn" align="middle" />
                <span>Senda fyrirspurn</span>
              </Grid.Column>
           </Grid.Row>
-          <Grid.Row centered>            
+          <Grid.Row centered className={styles.links}>
             <Grid.Column width={7} >
-            <span>Persónuvernd</span>
-            <span>Skilmálar</span>
-            <span>Vafrakökur</span>
-            <span>Ábendingar, kvartanir og hrós</span>
+            <a href='#'>Persónuvernd</a>
+            <span className={styles.slashDivider}>/</span>
+            <a href='#'>Skilmálar</a>
+            <span className={styles.slashDivider}>/</span>
+            <a href='#'>Vafrakökur</a>
+            <span className={styles.slashDivider}>/</span>
+            <a href='#'>Ábendingar, kvartanir og hrós</a>
             </Grid.Column>
-          <Grid.Column width={4} >
-          <span>Facebook</span>
-            <span>Youtube</span>
+          <Grid.Column width={3} className={styles.social}>
+            <img src={Facebook} alt="Facebook hlekkur" align="middle" />
+               <strong>Facebook</strong>
+            <img src={Youtube} alt="Youtube hlekkur" align="middle" />
+               <strong>Youtube</strong>
           </Grid.Column>
           </Grid.Row>
           <Grid.Row centered>
-          <Grid.Column width={11} >
-          <span>Vörður Tryggingar hf kt. 441099 - 3399  Vörður Líftryggingar hf kt. 620166 - 0229</span>
-            <span>Borgartún 25, 105 Reykjavík</span>
+          <Grid.Column textAlign='center' width={10} className={styles.companyInfo}>
+          <span>Vörður Tryggingar hf kt. 441099 - 3399</span>
+          <span>Vörður Líftryggingar hf kt. 620166 - 0229</span>
+          <span>Borgartún 25, 105 Reykjavík</span>
           </Grid.Column>
             </Grid.Row>
         </Grid>
